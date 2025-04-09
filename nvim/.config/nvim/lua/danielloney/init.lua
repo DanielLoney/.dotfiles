561f10b1893bf4bd81145affae5c68dc6dc91579
danielloney <danieljloney@gmail.com>
2025-04-08 20:40:22 -0400

    cmp and lsp

--- nvim/.config/nvim/lua/danielloney/init.lua
+++ nvim/.config/nvim/lua/danielloney/init.lua
@@ -9,20 +9,41 @@ local function on_attach(client)
     vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 end
 
+local function luals_on_attach(client)
+  if client.workspace_folders then
+    local path = client.workspace_folders[1].name
+    if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
+      return
+    end
+  end
+
+  client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+    runtime = {
+      -- Tell the language server which version of Lua you're using
+      -- (most likely LuaJIT in the case of Neovim)
+      version = 'LuaJIT'
+    },
+    -- Make the server aware of Neovim runtime files
+    workspace = {
+      checkThirdParty = false,
+      library = {
+        vim.env.VIMRUNTIME,
+        -- Depending on the usage, you might want to add additional paths here.
+        "${3rd}/luv/library",
+        "${3rd}/busted/library",
+      }
+      -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+      -- library = vim.api.nvim_get_runtime_file("", true)
+    }
+  })
+end
+
 vim.lsp.config['luals'] = {
   -- Command and arguments to start the server.
   cmd = { 'lua-language-server' },
-
-  -- Filetypes to automatically attach to.
   filetypes = { 'lua' },
   globals = {'vim'},
-
-  -- Sets the "root directory" to the parent directory of the file in the
-  -- current buffer that contains either a ".luarc.json" or a
-  -- ".luarc.jsonc" file. Files that share a root directory will reuse
-  -- the connection to the same LSP server.
   root_markers = { '.luarc.json', '.luarc.jsonc' },
-
   -- Specific settings to send to the server. The schema for this is
   -- defined by the server. For example the schema for lua-language-server
   -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
@@ -33,34 +54,7 @@ vim.lsp.config['luals'] = {
       }
     }
   },
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
-        return
-      end
-    end
-
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT'
-      },
-      -- Make the server aware of Neovim runtime files
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-          -- Depending on the usage, you might want to add additional paths here.
-          "${3rd}/luv/library",
-          "${3rd}/busted/library",
-        }
-        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-        -- library = vim.api.nvim_get_runtime_file("", true)
-      }
-    })
-  end,
+  on_init = luals_on_attach
 }
 
 vim.lsp.enable('luals')
